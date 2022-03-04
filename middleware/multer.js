aafe20365fa723d0b621390e24d9605b440c2a5f
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-03-04 21:57:00 +0200

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 const path = require('path')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../uploads'))
+        cb(null, path.join(__dirname, '../uploads/'))
         console.log('here123');
 
     },
