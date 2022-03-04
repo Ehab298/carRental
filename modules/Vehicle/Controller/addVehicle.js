b077c1f54b78b75245121259069f8d2b474e3152
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-03-04 02:49:20 +0200

    update

--- modules/Vehicle/Controller/addVehicle.js
+++ modules/Vehicle/Controller/addVehicle.js
@@ -2,18 +2,13 @@ const  vehicleModel =require('../../../DB/models/Vehicle')
 
 
  module.exports = async(req,res)=>{
-    const {brand,model,color,year,pricePerDay,transmissionType,VehicleRate,Car_Seat,airbag,seatbelts
-        ,ABS,sunroof,Parking_Sensors,Radio,Navigation_System,Bluetooth,Remote_Start,AC,Music_Player
-        ,Extra_Tyre,Charger,Fire_Extinguisher,First_Aid_Kit,CC,
-        Smoking_Preferences,companyID} = req.body;
+    const {brand,model,color, year,  VehicleRate,imageURL,pricePerDay,transmissionType, Car_Seat, airbag, seatbelts, ABS,sunroof,Parking_Sensors,  Radio, Navigation_System, Bluetooth,  Remote_Start, AC,  Music_Player, CC, Extra_Tyre,  Charger,  Fire_Extinguisher, First_Aid_Kit, Smoking_Preferences,companyID} = req.body;
     
     console.log(req.files);
     
  
-    const vehicleData= await vehicleModel.insertMany({brand,model,color,year,pricePerDay,imageURL:req.files,transmissionType,Car_Seat,VehicleRate,airbag,seatbelts
-        ,ABS,sunroof,Parking_Sensors,Radio,Navigation_System,Bluetooth,Remote_Start,AC,Music_Player
-        ,Extra_Tyre,Charger,Fire_Extinguisher,First_Aid_Kit,CC,
-        Smoking_Preferences,companyID})
+    const vehicleData= await vehicleModel.insertMany({brand,
+        model,color,year, VehicleRate, imageURL,pricePerDay,transmissionType, Car_Seat, airbag, seatbelts, ABS,  sunroof, Parking_Sensors,Radio, Navigation_System,Bluetooth,Remote_Start,AC, Music_Player,  CC,  Extra_Tyre,Charger, Fire_Extinguisher, First_Aid_Kit,Smoking_Preferences,companyID})
         res.status(200).json({message:"done",vehicleData})
 }
 
