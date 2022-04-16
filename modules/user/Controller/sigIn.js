c3bde090f2b49d5527b2e2842a045ffb482d53ff
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-04-16 21:35:45 +0200

    ojk

--- modules/user/Controller/sigIn.js
+++ modules/user/Controller/sigIn.js
@@ -21,7 +21,7 @@ module.exports = async (req, res) => {
 
             if(match) {
                 var token = jwt.sign({ id:user._id,role: user.role}, 'shhhhh');
-                res.status(200).json({message: "done",token, firstName: user.firstName,lastName:user.lastName,phone:user.phone ,email: user.email, role: user.role})
+                res.status(200).json({token, firstName: user.firstName,lastName:user.lastName,phone:user.phone ,email: user.email, role: user.role})
             }
             else{
                 res.status(400).json({message:"in-valid  user"});
