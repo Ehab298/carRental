73c05dce10110174895313d676aa5ab23310cf95
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-04-17 22:42:39 +0200

    puch

--- modules/Vehicle/Controller/getOnevehicle.js
+++ modules/Vehicle/Controller/getOnevehicle.js
@@ -5,19 +5,23 @@ const vehicleModel = require("../../../DB/models/Vehicle")
 module.exports=async(req,res)=>{
     
     debugger
-
-
- 
-        brands= req.query.brand
-        color=req.query.color
-        Vehicletype=req.query.Vehicletype
+        brand= req.query.brand?.trim();
+        color=req.query.color?.trim();
+        Vehicletype=req.query.Vehicletype?.trim();
+        year=req.query.year?.trim();
+        _id=req.query._id
+        let whereConditions = [];
+        if(Vehicletype) 
+          whereConditions.push({ Vehicletype: { $regex: '.*' + Vehicletype   + '.*' } });
+        if(color) 
+          whereConditions.push({ color: { $regex: '.*' + color   + '.*' } });
+        if(brand) 
+          whereConditions.push({ brand: { $regex: '.*' + brand   + '.*' } });
+          if(_id) 
+          whereConditions.push({ _id });
        
-  
-    
-        let data = await vehicleModel.find(
-            { Vehicletype: { $regex: '.*' + Vehicletype   + '.*' } }
-          
-          )
+
+        let data = await vehicleModel.find({$or: whereConditions})
         res.json({message:'success',data})
         
 };
