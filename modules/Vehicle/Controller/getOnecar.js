13e5e1ff1efb896df9dd8e5aa3f4604daaa3a350
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-04-18 13:52:08 +0200

    puch

--- modules/Vehicle/Controller/getOnecar.js
+++ modules/Vehicle/Controller/getOnecar.js
@@ -3,7 +3,7 @@ const vehicleModel = require("../../../DB/models/Vehicle")
 module.exports=async(req,res)=>{
     
     const _id = req.params.id;
-        let data = await vehicleModel.find({_id}).populate('companyID')
+        let data = await vehicleModel.findById({_id}).populate('companyID')
         res.status(200).json({message:"done",data})
         
 };
