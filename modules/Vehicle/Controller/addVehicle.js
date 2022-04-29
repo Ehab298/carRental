0216e1109cf444cdb08329a4d7492a6b4289175a
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-04-29 21:34:08 +0200

    puch

--- modules/Vehicle/Controller/addVehicle.js
+++ modules/Vehicle/Controller/addVehicle.js
@@ -1,10 +1,11 @@
 const  vehicleModel =require('../../../DB/models/Vehicle')
 
 try {
+    debugger
     module.exports = async(req,res)=>{
         const {brand,model,color, year,Vehicletype,Automatic,manual, doorsNumber,chairsNumber, VehicleRate,pricePerDay,transmissionType, Car_Seat, airbag, seatbelts, ABS,sunroof,Parking_Sensors,  Radio, Navigation_System, Bluetooth,  Remote_Start, AC,  Music_Player, CC, Extra_Tyre,  Charger,  Fire_Extinguisher, First_Aid_Kit, Smoking_Preferences,companyID} = req.body;
         
-        console.log(req.files);doorsNumber,chairsNumber
+       
         var images = [];
         req.files.forEach(function(file){
             ;
@@ -21,5 +22,5 @@ try {
     
 } catch (error) {
     console.log(error);
-    res.status(200).json({message:"error",error})
+    res.status(400).json({message:"error",error})
 }
