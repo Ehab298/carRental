96d2d29e497e00c2e3b351d2d931343a7e78d48c
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-05-04 23:34:06 +0200

    puch

--- modules/Vehicle/Controller/getOnevehicle.js
+++ modules/Vehicle/Controller/getOnevehicle.js
@@ -4,8 +4,10 @@ const vehicleModel = require("../../../DB/models/Vehicle")
 
 module.exports=async(req,res)=>{
         serch= req.query.serch?.trim()
+        Vehicletype= req.query.Vehicletype?.trim()
+        
          let data = await vehicleModel.find({$or:[
-         {Vehicletype:{ $regex: '.*' +serch+ '.*'}},
+         {Vehicletype:{ $regex: '.*' +Vehicletype+ '.*'}},
          {color:{ $regex: '.*' +serch+ '.*'}} ,
          {brand:{ $regex: '.*' +serch+ '.*'}},
          {model:{ $regex: '.*' +serch+ '.*'}},
