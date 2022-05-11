3854f380a494e9138b1412b0ea720833c0867346
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-05-11 19:57:11 +0200

    puch

--- modules/Vehicle/Controller/addVehicle.js
+++ modules/Vehicle/Controller/addVehicle.js
@@ -1,26 +1,23 @@
 const  vehicleModel =require('../../../DB/models/Vehicle')
-
-try {
     
-    module.exports = async(req,res)=>{
+module.exports = async(req,res)=>{
+    try {
         const {brand,model,color, year,Vehicletype,Automatic,manual, doorsNumber,chairsNumber, VehicleRate,pricePerDay,transmissionType, Car_Seat, airbag, seatbelts, ABS,sunroof,Parking_Sensors,  Radio, Navigation_System, Bluetooth,  Remote_Start, AC,  Music_Player, CC, Extra_Tyre,  Charger,  Fire_Extinguisher, First_Aid_Kit, Smoking_Preferences,companyID} = req.body;
-        
        
         var images = [];
+        if(req.files == null || req.files.length == 0)
+            res.status(400).json({message:"You must add files"});
         req.files.forEach(function(file){
-            ;
-            images.push(file.location);
+            if(file?.location !=null)
+                images.push(file.location);
         });
 
         const vehicleData= await vehicleModel.insertMany({brand,
-            model,color,year,Vehicletype,imageURL:images,doorsNumber,chairsNumber,Automatic,manual, VehicleRate,pricePerDay,transmissionType, Car_Seat, airbag, seatbelts, ABS,  sunroof, Parking_Sensors,Radio, Navigation_System,Bluetooth,Remote_Start,AC, Music_Player,  CC,  Extra_Tyre,Charger, Fire_Extinguisher, First_Aid_Kit,Smoking_Preferences,UserID:req.User.id,companyID})
-            res.status(200).json({message:"done",images,vehicleData})
-    }
-    
-    
-    
-    
-} catch (error) {
-    console.log(error);
-    res.status(400).json({message:"error",error})
-}
+            model,color,year,Vehicletype,imageURL:images,doorsNumber,chairsNumber,Automatic,manual, VehicleRate,pricePerDay,transmissionType, Car_Seat, airbag, seatbelts, ABS,  sunroof, Parking_Sensors,Radio, Navigation_System,Bluetooth,Remote_Start,AC, Music_Player,  CC,  Extra_Tyre,Charger, Fire_Extinguisher, First_Aid_Kit,Smoking_Preferences,companyID})
+        res.status(200).json({message:"done",images,vehicleData})
+    } 
+    catch (error) {
+        console.log(error);
+        res.status(400).json({message:"error",error})
+    }        
+}    
