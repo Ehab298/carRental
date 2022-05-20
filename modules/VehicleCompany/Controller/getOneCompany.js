4541c7d3ab18e34587d879ea340d0a97cb36881c
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-05-21 00:17:11 +0200

    puch

--- modules/VehicleCompany/Controller/getOneCompany.js
+++ modules/VehicleCompany/Controller/getOneCompany.js
@@ -5,6 +5,13 @@ module.exports=async(req,res)=>{
     const _id = req.params.id;
         let data = await CompanyModel.findOne({ _id})
         res.status(400).json({message:'success',data})
+
+
+
+
+
+
+        
         
 };
 
