d1831ea57fe0870fe9da9816d271b79856f2ef1e
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-05-22 19:35:54 +0200

    puch

--- modules/Booking/validations.js
+++ modules/Booking/validations.js
@@ -2,8 +2,8 @@ const Joi = require('joi');
 module.exports={
     addrent:{
         body:Joi.object().required().keys({
-            DateFrom:Joi.data().required(),
-            DateTo:Joi.date().required(),
+            DateFrom:Joi.string().required(),
+            DateTo:Joi.string().required(),
             VehicleID:Joi.string().required(),
             Pick_upLocation:Joi.string().required(),
             return_Location:Joi.string().required(),
