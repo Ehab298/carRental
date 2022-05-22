bd79c06e478bfd67eb70eb1f7ae3c40202f846f6
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-05-22 18:00:33 +0200

    puch

--- modules/Booking/validations.js
+++ modules/Booking/validations.js
@@ -5,6 +5,11 @@ module.exports={
             DateFrom:Joi.string().required(),
             DateTo:Joi.string().required(),
             VehicleID:Joi.string().required(),
+            Pick_upLocation:Joi.string().required(),
+            return_Location:Joi.string().required()
+         
+            
+    
         })
     },
   
