c1a96c7b0c3314ee20f1e9858da33c34c6f03534
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-05-22 21:08:57 +0200

    puch

--- modules/Booking/Controller/Booking.js
+++ modules/Booking/Controller/Booking.js
@@ -4,17 +4,17 @@ const  RentModel =require('../../../DB/models/Booking')
  module.exports = async(req,res)=>{
     const {Pick_upLocation,return_Location,DateFrom,DateTo,message,VehicleID} = req.body;
     
-        let overlappedRent = await RentModel.find({VehicleID:VehicleID, DateFrom: { $lte: DateTo }, DateTo: { $gte: DateFrom } }).select();
-        //start date > today date && start date <= from date   
-        if(new Date(DateFrom) < new Date() || new Date(DateFrom) > new Date(DateTo))
-            res.json({message:"Date from not smaller than Date To or Date From not larger than Current Date"});
-        //get overlaped rents, (StartA <= EndB) and (EndA >= StartB)
-        else if(overlappedRent.length > 0)
-        res.json({message:`Vehicle already rentet in this period,From ${overlappedRent[0].DateFrom.toISOString()} TO ${overlappedRent[0].DateTo.toISOString()}`});  
-        else{
+        // let overlappedRent = await RentModel.find({VehicleID:VehicleID, DateFrom: { $lte: DateTo }, DateTo: { $gte: DateFrom } }).select();
+        // //start date > today date && start date <= from date   
+        // if(new Date(DateFrom) < new Date() || new Date(DateFrom) > new Date(DateTo))
+        //     res.json({message:"Date from not smaller than Date To or Date From not larger than Current Date"});
+        // //get overlaped rents, (StartA <= EndB) and (EndA >= StartB)
+        // else if(overlappedRent.length > 0)
+        // res.json({message:`Vehicle already rentet in this period,From ${overlappedRent[0].DateFrom.toISOString()} TO ${overlappedRent[0].DateTo.toISOString()}`});  
+        // else{
             const Rend= await RentModel.insertMany({Pick_upLocation,return_Location,DateFrom,DateTo,message,VehicleID,UserID: req.User.id})
             res.status(200).json({message:"done",Rend});
-        }
+        // }
 }
 
 
