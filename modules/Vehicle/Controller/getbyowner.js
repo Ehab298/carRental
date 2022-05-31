c40acb1fdf0c4e5aa4e5e5a0990b38fb557f8ddd
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-05-31 15:23:55 +0200

    puch

--- modules/Vehicle/Controller/getbyowner.js
+++ modules/Vehicle/Controller/getbyowner.js
@@ -2,30 +2,19 @@ const CompanyModel = require("../../../DB/models/VehicleCompany")
 const vehicleModel = require("../../../DB/models/Vehicle")
 const userModel = require("../../../DB/models/User")
 module.exports=async(req,res)=>{
+
+
+    const _id = req.params.id;
+    let company = await CompanyModel.findOne({ _id})
     
-    const _id = req.User.id;
-    await userModel.findOne({_id})
-            res.status(200).json({message:'success',obj})
-//         if(data) {
-            
+    if (company) {
 
+     
 
-// // let companyID =data.conpanyId
-// // console.log(company);
-// // console.log(companyID);
-
-
-
-
-//         //   let data =  await vehicleModel.find({companyID:_id}).populate('companyID')
-//             res.status(200).json({message:'success',obj})
-//         } else {
-//             res.status(400).json({message:'err'})
-//         }
-        
-
-
-        
-};
-
-
+        let data =  await vehicleModel.find({companyID}).populate('companyID')
+            res.status(200).json({message:'success',data})
+        } else {
+            res.status(400).json({message:'err'})
+        }
+}
+    
