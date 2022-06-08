fbc311f500b7dea79f67935321345d138c0a8185
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-06-08 20:05:56 +0200

    puch

--- modules/user/Controller/sigIn.js
+++ modules/user/Controller/sigIn.js
@@ -6,14 +6,14 @@ module.exports = async (req, res) => {
 
     const { email,password } = req.body;
     const user = await userModel.findOne({ email });
-    console.log(user);
+   
     if (!user) {
-        json({message:'in-valid  user'})
+        res.status(400).json({message:'in-valid user'})
     } 
     
     else {
         if (!user.confirm) {
-             json({ message: "please confirmed email" })
+            res.json({ message: "please confirmed email" })
         }else
 
         {
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
                 res.status(200).json({message:"done",token,id:user._id, firstName: user.firstName,lastName:user.lastName,phone:user.phone ,email: user.email, role: user.role,CompanuID:user.conpanyId})
             }
             else{
-                res.status(400).json({message:"in-valid  user"});
+                res.status(400).json({message:"in-valid user"});
             }}
 
       
