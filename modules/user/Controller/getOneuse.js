ec9cba5e854cab60cbd5f7a0e42c52afdce18d7e
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-06-09 00:35:57 +0200

    puch

--- modules/user/Controller/getOneuse.js
+++ modules/user/Controller/getOneuse.js
@@ -40,13 +40,13 @@ const Restpassword= async (req, res) => {
             if (!user) {
                 res.status(400).json({ error: "in-vali user" })
             }    else{
-                let {password,cpassword} =  req.body
+                let {email,password,cpassword} =  req.body
                 bcrypt.hash(password, 8, async function(err, hash) {
                     if (err) {
                         res.json({message:"hash err"})
                     } else {
                       
-                    const updataUser = await userModel.findOneAndUpdate({email:body1[0]},{password:hash},{new:true})
+                    const updataUser = await userModel.findOneAndUpdate({email},{password:hash},{new:true})
                     res.status(200).json({message:"done",updataUser});
                 
                     }})
