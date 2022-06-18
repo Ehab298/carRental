2f6899e09bed65c174f70c8aded892835e3eb72f
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-06-18 16:38:28 +0200

    puch

--- modules/Booking/Controller/getAllbooking.js
+++ modules/Booking/Controller/getAllbooking.js
@@ -3,7 +3,7 @@ const bookingModel = require("../../../DB/models/Booking")
 module.exports=async(req,res)=>{
     
      
-        let data = await bookingModel.find()
+        let data = await bookingModel.find().populate('UserID')
         res.json({message:'success',data})
         
 };
--- modules/Booking/Controller/getOnebooking.js
+++ modules/Booking/Controller/getOnebooking.js
@@ -3,7 +3,7 @@ const bookingModel = require("../../../DB/models/Booking")
 module.exports=async(req,res)=>{
     
     const _id = req.params.id;
-        let data = await bookingModel.findOne({ _id})
+        let data = await bookingModel.findOne({ _id}).populate('UserID')
         res.json({message:'success',data})
         
 };
