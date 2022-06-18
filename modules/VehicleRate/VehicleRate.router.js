8cfd220d3839a8648e80f58af4bbca66db404002
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-06-18 06:51:21 +0200

    puch

--- modules/VehicleRate/VehicleRate.router.js
+++ modules/VehicleRate/VehicleRate.router.js
@@ -6,11 +6,10 @@ const {authentications,authorization} = require('../../middleware/authintication
 const getafterDateto = require('./Controller/getafterDateto');
 const { VehicleRate } = require('./validation')
 const handelValidations = require('../../middleware/handelValidations');
-const VehicleRateController =require('./Controller/VehicleRate');
+
 const updateVehicleRateController =require('./Controller/updateVehicleRate');
-const getallrate =require('./Controller/getAlluserRate');
-router.post("/VehicleRate",authentications(),handelValidations(VehicleRate),VehicleRateController)
-router.get("/getAlluserRate",getallrate)
+
+
 
 router.get('/userbookingRate/:UserID',getafterDateto)
 router.post("/updateVehicleRate",authentications(),handelValidations(VehicleRate),updateVehicleRateController)
