698ae2e740cb7029e0b051b1d0ea4f36c84f8600
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-06-24 16:37:22 +0200

    sdd

--- modules/Booking/Controller/Booking.js
+++ modules/Booking/Controller/Booking.js
@@ -4,14 +4,14 @@ const car = require("../../../DB/models/Vehicle")
  module.exports = async(req,res)=>{
     const {Pick_upLocation,return_Location,DateFrom,DateTo,message,VehicleID,status} = req.body;
     
-        // let overlappedRent = await RentModel.find({VehicleID:VehicleID, DateFrom: { $lte: DateTo }, DateTo: { $gte: DateFrom } }).select();
-        // //start date > today date && start date <= from date   
-        // if(new Date(DateFrom) < new Date() || new Date(DateFrom) > new Date(DateTo))
-        //     res.json({message:"Date from not smaller than Date To or Date From not larger than Current Date"});
-        // //get overlaped rents, (StartA <= EndB) and (EndA >= StartB)
-        // else if(overlappedRent.length > 0)
-        // res.json({message:`Vehicle already rentet in this period,From ${overlappedRent[0].DateFrom.toISOString()} TO ${overlappedRent[0].DateTo.toISOString()}`});  
-        // else{
+        let overlappedRent = await RentModel.find({VehicleID:VehicleID, DateFrom: { $lte: DateTo }, DateTo: { $gte: DateFrom } }).select();
+        //start date > today date && start date <= from date   
+        if(new Date(DateFrom) < new Date() || new Date(DateFrom) > new Date(DateTo))
+            res.json({message:"Date from not smaller than Date To or Date From not larger than Current Date"});
+        //get overlaped rents, (StartA <= EndB) and (EndA >= StartB)
+        else if(overlappedRent.length > 0)
+        res.json({message:`Vehicle already rentet in this period,From ${overlappedRent[0].DateFrom.toISOString()} TO ${overlappedRent[0].DateTo.toISOString()}`});  
+        else{
 
             let data = await car.find({_id:VehicleID})
 
@@ -20,7 +20,7 @@ const car = require("../../../DB/models/Vehicle")
        
        
                        
-        //  }
+         }
 }
 
 
