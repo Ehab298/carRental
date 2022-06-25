e923860c3a63ed2b7d4880b896405941e37527f6
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2022-06-25 23:18:47 +0200

    puch

--- modules/Booking/Controller/UpdateBooking.js
+++ modules/Booking/Controller/UpdateBooking.js
@@ -7,8 +7,8 @@ const bookingModel = require('../../../DB/models/Booking');
 module.exports= async(req, res) => {
     const {Pick_upLocation,return_Location,DateFrom,DateTo,status,VehicleID} = req.body;
     const _id = req.params.id
-    const newBooking =  await bookingModel.updateOne({ _id: _id }, {Pick_upLocation,return_Location,DateFrom,DateTo,status,VehicleID},{new:true}).then(() => {
-        res.status(200).json({message:"done"});
-    })
+    const newBooking =  await bookingModel.findOneAndUpdate({ _id: _id }, {Pick_upLocation,return_Location,DateFrom,DateTo,status,VehicleID},{new:true})
+        res.status(200).json({message:"done",newBooking});
+    
    
 };
