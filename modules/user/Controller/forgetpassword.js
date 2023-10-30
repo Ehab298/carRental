f0ceb7194f3f72b3f7ae42458af44faf4ebc2071
Ehab298 <95766738+Ehab298@users.noreply.github.com>
2023-10-31 01:01:52 +0200

    puch9

--- modules/user/Controller/forgetpassword.js
+++ modules/user/Controller/forgetpassword.js
@@ -71,7 +71,7 @@ module.exports =async(req,res,next)=>{
                                                     password has been generated for you. To reset your password, click the
                                                     following link and follow the instructions.
                                                 </p>
-                                                <a href="https://car-rental-eg.herokuapp.com/getOneuser/${token}"
+                                                <a href="http://localhost:4400/getOneuser/${token}"
                                                     style="background:#3320e2;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
                                                     Password</a>
                                             </td>
